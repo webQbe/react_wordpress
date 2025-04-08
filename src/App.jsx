c34ac1281059430502b2fb222ef924e2f6bf24b5
webQbe <webcubesolutionslk@gmail.com>
2025-04-08 18:39:59 +0530

    Added proxy to vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
 
   return (
     <div className="App">
-      <h1>App</h1>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, }
+  server: { 
+        proxy: {
+          '/wp-json': {
+            target: 'http://localhost:8000',
+            changeOrigin: true,
+            secure: false,
+          }
+        }
+   }
 })
