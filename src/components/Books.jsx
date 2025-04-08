0aa244d34e0203aedc9a110830be3ca0714b63c7
webQbe <webcubesolutionslk@gmail.com>
2025-04-08 19:51:38 +0530

    Fetched data from Wordpress backend & displayed on frontend

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -17,16 +17,33 @@ const Books = () => {
         setIsLoaded(true);  // Set isLoaded to true
       })
       // If there's an error, it logs it to the console
-      .catch(err => console.log(err));  
+      .catch(err => console.log(err));
 
   }, []); // Run once when component mounts
 
-  console.log({ books, isLoaded });
+  // If !isLoaded is false show Loading...
+  if (!isLoaded) return <h3>Loading...</h3>
 
   return (
     <div>
+        { books.length > 0 ? // If books.length is greater than 0, run the code
+            ( 
+              // Map over books array
+              books.map(book => (
+                /* Render <div> for each book with its title */
+                <div key={ book.id }>
+                    <h4>{ book.title.rendered }</h4>
+                </div>
+             ))
+            ) : 
+            (  
+                /* If there are no books */
+                <p>No books found.</p>
+            )
+        }
     </div>
-  );
+  )
+
 };
 
 export default Books;
