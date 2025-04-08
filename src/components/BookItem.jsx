5c90d0cafc4837de0f9228fa958c7edbd894518b
webQbe <webcubesolutionslk@gmail.com>
2025-04-08 20:44:38 +0530

    Rendered HTML in the API response

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -3,10 +3,23 @@ import React from 'react'
 
 const BookItem = ({ book }) => { // Receive a book prop
 
-  /* Render title.rendered field from  WordPress API response */
+  /* Render fields from WordPress API response */
   return (
     <div>
         <h2>{ book.title.rendered }</h2>
+        <div 
+            dangerouslySetInnerHTML={{ // Trust this HTML & render it directly (Removes HTML tags)
+                __html: book.excerpt.rendered 
+            }} 
+                /* This prop is called “dangerously” because you're telling React:
+
+                    “Hey, I trust this HTML, so render it directly.”
+
+                 If the HTML includes something malicious (like a <script> tag), it could harm the user — so only use it with trusted sources (like your WordPress site).
+                
+                 If you don't want to use dangerouslySetInnerHTML, you could strip tags or use a library like DOMPurify or show plain text instead — but you’ll lose formatting. */
+        />
+    
     </div>
   )
 }
